57e7afb220559c3018ce1ae18c0ddd413f1989a4
Masayuki Abe <marchin.1989@gmail.com>
2021-01-03 23:59:18 +0900

    JankenControllerから、Serviceに処理を抽出(day6)

---
+++ app/src/main/java/com/example/janken/service/PlayerService.java
@@ -0,0 +1,32 @@
+package com.example.janken.service;
+
+import com.example.janken.model.Player;
+import lombok.val;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class PlayerService {
+
+    private static final String PLAYERS_CSV = ServiceConfigurations.DATA_DIR + "players.csv";
+
+    public Player findPlayerById(long playerId) throws IOException {
+        try (val stream = Files.lines(Paths.get(PLAYERS_CSV), StandardCharsets.UTF_8)) {
+            return stream
+                    .map(line -> {
+                        val values = line.split(ServiceConfigurations.CSV_DELIMITER);
+                        val id = Long.parseLong(values[0]);
+                        val name = values[1];
+                        return new Player(id, name);
+                    })
+                    // ID で検索
+                    .filter(p -> p.getId() == playerId)
+                    .findFirst()
+                    .orElseThrow(() -> {
+                        throw new IllegalArgumentException("Player not exist. playerId = " + playerId);
+                    });
+        }
+    }
+}
