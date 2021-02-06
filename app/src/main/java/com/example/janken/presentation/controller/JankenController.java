ccb32886de282cf34a0428c9c2cbbfc1d98274d4
Masayuki Abe <marchin.1989@gmail.com>
2021-02-06 18:47:40 +0900

    viewをpresentation配下に移動

--- app/src/main/java/com/example/janken/presentation/controller/JankenController.java
+++ app/src/main/java/com/example/janken/presentation/controller/JankenController.java
@@ -4,7 +4,7 @@ import com.example.janken.application.service.JankenApplicationService;
 import com.example.janken.application.service.PlayerApplicationService;
 import com.example.janken.domain.model.janken.Hand;
 import com.example.janken.domain.model.player.Player;
-import com.example.janken.framework.View;
+import com.example.janken.presentation.view.View;
 import com.example.janken.registry.ServiceLocator;
 import lombok.val;
 
